30fd4dda5b4e64b0da74c7f9aae4425195cced2e
michael k <mking@arikel.net>
2019-05-18 16:02:27 -0400

    fix(build): use correct go get parameters

--- build/install_protoc.sh
+++ build/install_protoc.sh
@@ -7,7 +7,6 @@ wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/$
 unzip ${PROTOC_FILENAME}
 bin/protoc --version
 popd
-go get -u google.golang.org/genproto@v0.0.0-20190516172635-bb713bdc0e52
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
-go get -u github.com/grpc-ecosystem/grpc-gateway@v1.9.0
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@1.9.0
 go get -u github.com/golang/protobuf/protoc-gen-go@v1.3.1
