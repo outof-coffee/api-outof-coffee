197cbd4385dab20096562ac585eebe1c588dbf03
michael k <mking@arikel.net>
2019-05-18 15:27:29 -0400

    fix(build): install protobufs

---
+++ build/install_protoc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PROTOBUF_VERSION=3.5.1
+PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
+pushd /home/travis
+wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
+unzip ${PROTOC_FILENAME}
+bin/protoc --version
+popd
