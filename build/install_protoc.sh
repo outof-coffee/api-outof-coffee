1b9bf3940bfc0e549fe6df650382fe4db82d80f1
michael k <mking@arikel.net>
2019-05-18 15:54:08 -0400

    fix(build) might want to include the actual GO generator plugin for protoc...

--- build/install_protoc.sh
+++ build/install_protoc.sh
@@ -10,3 +10,4 @@ popd
 go get -u google.golang.org/genproto@v0.0.0-20190516172635-bb713bdc0e52
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 go get -u github.com/grpc-ecosystem/grpc-gateway@v1.9.0
+go get -u github.com/golang/protobuf/protoc-gen-go@v1.3.1
